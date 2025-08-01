4e2d2bf876c0168e05ff07234f13142bfc3be21b
joric <joric@users.noreply.github.com>
2025-08-01 06:58:32 +0500

    updated files

---
+++ src/controls/search-control/search-control.js
@@ -0,0 +1,112 @@
+class SearchControl {
+
+  _historyEntryName = 'searchHistory';
+  _maxHistorySize = 50;
+  _masStringLength = 100;
+
+  _handleSubmit = () => {
+  }
+
+  _clear = event => {
+    if (event) event.preventDefault();
+    this._input.value = '';
+    this._handleSubmit();
+  }
+
+  _updateDropdown = () => {
+    let html = this._items.map(text => `<li tabindex=0><span class="search-item-text">${text}</span><button class="search-button search-delete">&times;</button></li>`).join('');
+    document.querySelector('.search-list').innerHTML = html;
+    localStorage.setItem(this._historyEntryName, JSON.stringify(this._items));
+  }
+
+  _submit = event => {
+      if (event) event.preventDefault();
+      this._handleSubmit();
+    
+      let value = this._input.value.trim().slice(0, this._maxStringLength);
+      if (!value) return false;
+
+      let index = this._items.indexOf(value);
+      if (index !== -1) {
+        this._items.splice(index, 1);
+      }
+
+      this._items.unshift(value);
+      this._items = this._items.slice(0, this._maxHistorySize);
+      this._updateDropdown();
+
+      return false;
+  };
+
+  constructor(layer, options) {
+
+    let autofocus = false;
+
+    const innerHTML = `
+      <form class="search-form">
+      <div class="search-container">
+        <div class="search-input-container">
+          <input type="text" class="search-input" name="q" tabindex=1 placeholder="Search..."${autofocus ? 'autofocus':''}>
+          <button type="submit" class="search-button search-submit" tabindex=-1 title="Search">&#128269;&#xFE0E;</button>
+          <button type="button" class="search-button search-clear" tabindex=0 title="Cancel">&times;</button>
+        </div>
+        <div class="search-list-container"><ul class="search-list"></ul></div>
+      </div>
+      </form>
+    `;
+
+    document.body.insertAdjacentHTML('beforeend', innerHTML);
+
+    this._input = document.querySelector('.search-input');
+    this._items = JSON.parse(localStorage.getItem(this._historyEntryName) || "[]");
+
+    document.querySelector('.search-clear').onclick = this._clear;
+    document.querySelector('.search-form').onsubmit = this._submit;
+
+    document.querySelector('.search-list').onclick = event => {
+
+      if (event.target.classList.contains('search-delete')) {
+        let item = event.target.parentElement;
+        let text = item.firstChild.textContent;
+        this._items = this._items.filter(s => s !== text);
+        this._updateDropdown();
+        this._input.focus();
+      } else {
+        let item = event.target;
+        let text = item.firstChild.textContent;
+        this._input.value = text;
+        this._submit();
+      }
+
+    };
+
+    this._input.addEventListener("change", event => {
+      if (this._input.value=='') this._handleSubmit();
+    });
+
+
+    this._input.addEventListener("focus", event => {
+      this._input.select();
+    });
+
+    this._input.addEventListener("keydown", event => {
+      if (event.key === "Escape") {
+        this._clear();
+      }
+    });
+
+    this._updateDropdown();
+
+    // clicking on clear/search changes focus and starts animation - can we mitigate that in css?
+    let handleFocus = event => {
+      if (document.activeElement !== this._input) {
+        event.preventDefault();
+      }
+      this._clear();
+    };
+
+    document.querySelector('.search-clear').onmousedown = handleFocus;
+    document.querySelector('.search-clear').ontouchstart = handleFocus;
+
+  }
+}
