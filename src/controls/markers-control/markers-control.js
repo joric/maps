8f4de9f3835e0c8549673a6e00c51cd43763b737
joric <joric@users.noreply.github.com>
2025-08-02 15:15:54 +0500

    updated files

---
+++ src/controls/markers-control/markers-control.js
@@ -0,0 +1,36 @@
+class MarkersControl {
+  constructor(counters, options) {
+
+    const cmpAlphaNum = (a,b) => a[0].localeCompare(b[0], 'en', { numeric: true });
+    const translate = options?.translate || (s => s[0].toUpperCase()+s.slice(1));
+    let cmpGroup = (a,b)=>(options?.weights?.[b]??0-options?.weights?.[a]??0)||cmpAlphaNum(a,b);
+
+    document.querySelector('.markers-control')?.remove();
+
+    document.body.append( Object.assign(document.createElement('ul'), {
+      className: 'markers-control',
+      innerHTML: Object.keys(counters).sort(cmpGroup).map(group =>
+        `<li tabindex="0"><div class="markers-control-group" data-name="${group}">${translate(group)}</div><ul>`+
+        Object.entries(counters[group]).sort(cmpAlphaNum).map(([type, count]) =>
+          `<li tabindex="0" class="markers-control-item" data-name="${type}"><i class="${options?.icons?.[type]?.class}"></i><span>${translate(type)}</span><span>${count}</span></li>`
+        ).join('')
+        +`</ul></li>`
+      ).join('')
+    }));
+
+    document.querySelectorAll('.markers-control .markers-control-group').forEach(el=>{
+      el.addEventListener('click', e => {
+        options.groupCallback ? options.groupCallback(e.target.dataset.name) : console.log('groupCallback', e);
+      })
+    })
+
+    document.querySelectorAll('.markers-control .markers-control-item').forEach(el=>{
+      el.addEventListener('click', e => {
+        options.itemCallback ? options.itemCallback(e.target.dataset.name) :  console.log('itemCallback', e);
+      })
+    })
+
+  }
+}
+
+
